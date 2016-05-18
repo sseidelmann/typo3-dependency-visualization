6ee3c9b7b2539d4718421d757a3c598a0c9489ca
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2016-05-18 14:06:27 +0200

    [TASK] ADDED the missing functions

--- src/Graph.php
+++ src/Graph.php
@@ -6,6 +6,8 @@
  */
 
 namespace Sseidelmann\Typo3DependencyVisualization;
+use Fhaculty\Graph\Vertex;
+use Graphp\GraphViz\GraphViz;
 use Ulrichsg\Getopt\Getopt;
 use Ulrichsg\Getopt\Option;
 
@@ -23,25 +25,65 @@ class Graph
     const VERSION = '1.0';
 
     /**
+     * Defines the color of the version.
+     * @var string
+     */
+    const GRAPH_COLOR_VERSION = 'gray';
+
+    /**
      * Saves the getopt instance.
      * @var Getopt
      */
     private $getopt;
 
     /**
+     * Saves the edges.
+     * @var Vertex[]
+     */
+    private $graphNodes = array();
+
+    /**
+     * Saves the graph
+     * @var \Fhaculty\Graph\Graph
+     */
+    private $graph;
+
+    /**
+     * Saves the graphviz
+     * @var GraphViz
+     */
+    private $graphviz;
+
+    /**
+     * Display the typo3 constraints (eg. extbase) in graph
+     * @var bool
+     */
+    private $displayTypoContraints = true;
+
+    /**
+     * Saves the options.
+     * @var array
+     */
+    private $options = array();
+
+    /**
      * Constructs the Graph
      */
     public function __construct()
     {
-        $this->getopt = new Getopt(array(
+        $this->graph    = new \Fhaculty\Graph\Graph();
+        $this->graphviz = new GraphViz();
+        $this->getopt   = new Getopt(array(
             new Option(null, 'extpath', Getopt::REQUIRED_ARGUMENT),
+            new Option(null, 'extpattern', Getopt::REQUIRED_ARGUMENT),
+            new Option(null, 'versions'),
             new Option(null, 'version')
         ));
         $this->getopt->setBanner(implode(PHP_EOL, array(
-            "\033[0;32mGraph\033[0m version " . self::VERSION,
-            "\033[0;31m%s\033[0m",
-            "Usage: graph [options]"
-        )) . PHP_EOL);
+                "\033[0;32mGraph\033[0m version " . self::VERSION,
+                "\033[0;31m%s\033[0m",
+                "Usage: graph [options]"
+            )) . PHP_EOL);
     }
 
     /**
@@ -63,99 +105,269 @@ class Graph
                 throw new \Exception('Option \'extpath\' must be given');
             }
 
-            return $options;
+            $this->options = $options;
         } catch (\Exception $exception) {
             echo sprintf($this->getopt->getBanner(), PHP_EOL . $exception->getMessage() . PHP_EOL);
             exit(0);
         }
     }
 
+    /**
+     * Generate the graph
+     * @return void
+     */
     public function generate()
     {
-        $options = $this->parseArguments();
+        $this->parseArguments();
 
-        $path = realpath($options['path']) . DIRECTORY_SEPARATOR;
-
-        $files = glob($path . 'wfp2_*/ext_emconf.php');
-
-        print_r($files);
+        $path = realpath($this->options['extpath']) . DIRECTORY_SEPARATOR;
+        echo sprintf('Scanning the path %s for extensions', $path) . PHP_EOL;
 
 
-        return;
-        $path = realpath(dirname(__FILE__) . '/typo3conf/ext/') . DIRECTORY_SEPARATOR;
+        $files = $this->getExtensionManagerConfigurationFiles($path);
+        $constraints = $this->getConstraints($files);
 
+        $this->createNodesForConstraints($constraints);
+        $this->createEdgesForConstraints($constraints);
+
+        if (isset($this->options['versions'])) {
+            $this->createVersionConstraints($constraints);
+        }
+
+
+        $this->getGraph()->setAttribute('graphviz.graph.ratio', '0.2');
+        $this->getGraphviz()->display($this->getGraph());
+    }
+
+    /**
+     * Creates the nodes for the constraints.
+     * @param array $constraints
+     * @return void
+     */
+    private function createNodesForConstraints(array $constraints)
+    {
+        foreach (array_keys($constraints) as $ext) {
+            $this->createNode($ext);
+        }
+    }
+
+    private function createVersionConstraints(array $constraints)
+    {
+        foreach ($constraints as $ext => $depends) {
+            foreach ($depends as $depend => $version) {
+                $version = ($version == '') ? '*' : $version;
+                $versionNodeId = sprintf('%s_%s', $depend, $version);
+                if (!$this->findGraphNode($versionNodeId)) {
+                    $this->createVersionNode($versionNodeId, sprintf('%s: %s', $depend, $version));
+                    $this->setGraphNodeColor($versionNodeId, $this->getGraphNodeColor($depend));
+                    $this->createVersionEdge($depend, $versionNodeId);
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates the node with a name
+     * @param string $id
+     * @param string $name
+     * @return void
+     */
+    private function createNode($id, $name = null)
+    {
+        $this->addGraphNode(
+            $id,
+            $this->getGraph()->createVertex($name !== null ? $name : $id)
+        );
+    }
+
+    private function createVersionNode($id, $name)
+    {
+        $this->createNode($id, $name);
+        $this->findGraphNode($id)->setAttribute('graphviz.shape', 'rect');
+        $this->findGraphNode($id)->setAttribute('graphviz.color', self::GRAPH_COLOR_VERSION);
+    }
+
+    /**
+     * Create the edges for constraints.
+     * @param array $constraints
+     * @return void
+     */
+    private function createEdgesForConstraints(array $constraints)
+    {
+        foreach ($constraints as $ext => $depends) {
+            foreach ($depends as $depend => $version) {
+                if (false === $this->findGraphNode($depend) && $this->displayTypoContraints) {
+                    $this->createVersionNode($depend, $depend);
+                }
+
+                if ($node = $this->findGraphNode($depend)) {
+                    $this->createEdge($ext, $depend);
+                }
+            }
+        }
+    }
+
+    /**
+     * Create the edges from one node to another
+     * @param string $from
+     * @param string $to
+     */
+    private function createEdge($from, $to)
+    {
+        if (($nodeFrom = $this->findGraphNode($from)) !== false &&
+            ($nodeTo = $this->findGraphNode($to)) !== false
+        ) {
+            $edge = $nodeFrom->createEdgeTo($nodeTo);
+            $edge->setAttribute('graphviz.color', $this->getGraphNodeColor($from));
+            $edge->setAttribute('graphviz.splines', 'false');
+        }
+    }
+
+    /**
+     * Create the edges from one node to another
+     * @param string $from
+     * @param string $to
+     */
+    private function createVersionEdge($from, $to)
+    {
+        if (($nodeFrom = $this->findGraphNode($from)) !== false &&
+            ($nodeTo = $this->findGraphNode($to)) !== false
+        ) {
+            $edge = $nodeFrom->createEdge($nodeTo);
+            $edge->setAttribute('graphviz.color', self::GRAPH_COLOR_VERSION);
+            $edge->setAttribute('graphviz.splines', 'false');
+        }
+    }
+
+    /**
+     * Returns the files for extension configuration by given ext path.
+     * @param string $path
+     * @return array
+     */
+    private function getExtensionManagerConfigurationFiles($path)
+    {
+        $pattern = isset($this->options['extpattern']) ? $this->options['extpattern'] : '*';
+        $path    = sprintf('%s%s/ext_emconf.php', $path, $pattern);
+
+        return glob($path);
+    }
+
+
+    /**
+     * Returns the constraints.
+     * @param array $files
+     * @return array
+     */
+    private function getConstraints($files)
+    {
         $constraints = array();
         foreach ($files as $file) {
             $pathInfo = pathinfo($file);
-            $extName  = @end(explode(DIRECTORY_SEPARATOR, $pathInfo['dirname']));
+            $extName = @end(explode(DIRECTORY_SEPARATOR, $pathInfo['dirname']));
 
-            $constraints[$extName] = getDepends($extName, $file);
+            $constraints[$extName] = $this->extractDependencies($extName, $file);
         }
 
+        return $constraints;
+    }
 
-        $graph = new \Fhaculty\Graph\Graph();
+    /**
+     * Extracts the deps.
+     * @param string $extName
+     * @param string $file
+     * @return array
+     */
+    private function extractDependencies($extName, $file)
+    {
+        $_EXTKEY = $extName;
+        include $file;
+
+        $config = $EM_CONF[$_EXTKEY];
+        if (isset($config['constraints'])) {
+            return $config['constraints']['depends'];
+        }
+        return array();
+    }
+
+    private function getRandColor()
+    {
+        $colors = array(
+            'blue',
+            'red',
+            'coral',
+            'crimson',
+            'darkslateblue',
+            'cornflowerblue',
+            'aquamarine1',
+            'darkgreen'
+        );
+
+        return $colors[rand(0, count($colors) - 1)];
+    }
 
 
+    /**
+     * Returns the graph.
+     * @return \Fhaculty\Graph\Graph
+     */
+    private function getGraph()
+    {
+        return $this->graph;
+    }
 
-        /* @var $graphEdges \Fhaculty\Graph\Vertex[] */
-        $graphEdges = array();
+    /**
+     * Returns the graphviz
+     * @return GraphViz
+     */
+    public function getGraphviz()
+    {
+        return $this->graphviz;
+    }
 
+    /**
+     * Adds a graph edge.
+     * @param string $id
+     * @param Vertex $vertex
+     */
+    private function addGraphNode($id, Vertex $vertex)
+    {
+        $this->graphNodes[$id] = $vertex;
+        $this->setGraphNodeColor($id, $this->getRandColor());
+    }
 
-        foreach ($constraints as $ext => $depends) {
-            $graphEdges[$ext] = $graph->createVertex($ext);
-            $graphEdges[$ext]->setAttribute('graphviz.color', getRandColor());
+    /**
+     * Returns the graph edge.
+     * @param string $id
+     * @return Vertex|null
+     */
+    private function findGraphNode($id)
+    {
+        return isset($this->graphNodes[$id]) ? $this->graphNodes[$id] : false;
+    }
+
+    /**
+     * Sets the graph color
+     * @param string $id
+     * @param string $color
+     */
+    private function setGraphNodeColor($id, $color)
+    {
+        if ($node = $this->findGraphNode($id)) {
+            $node->setAttribute('graphviz.color', $color);
+        }
+    }
+
+    /**
+     * Returns the graph node color.
+     * @param string $id
+     * @return string
+     */
+    private function getGraphNodeColor($id)
+    {
+        if ($node = $this->findGraphNode($id)) {
+            return $node->getAttribute('graphviz.color');
         }
 
-
-
-        foreach ($constraints as $ext => $depends) {
-            foreach ($depends as $depend => $version) {
-                if (!isset($graphEdges[$depend])) {
-                    // $graphEdges[$depend] = $graph->createVertex($depend);
-                    // $graphEdges[$depend]->setAttribute('graphviz.color', getRandColor());
-                }
-
-                if (isset($graphEdges[$depend])) {
-                    $edge = $graphEdges[$ext]->createEdgeTo($graphEdges[$depend]);
-                    $edge->setAttribute('graphviz.color', $graphEdges[$ext]->getAttribute('graphviz.color'));
-                    $edge->setAttribute('splines', 'false');
-                }
-            }
-        }
-
-
-        $graphviz = new \Graphp\GraphViz\GraphViz();
-        $graphviz->display($graph);
-
-        echo $graphviz->createScript($graph);
-
-
-        function getDepends($extName, $file)
-        {
-            $_EXTKEY = $extName;
-            include $file;
-
-            $config = $EM_CONF[$_EXTKEY];
-            if (isset($config['constraints'])) {
-                return $config['constraints']['depends'];
-            }
-            return array();
-        }
-
-        function getRandColor()
-        {
-            $colors = array(
-                'blue',
-                'red',
-                'coral',
-                'crimson',
-                'darkslateblue',
-                'cornflowerblue',
-                'aquamarine1',
-                'darkgreen'
-            );
-
-            return $colors[rand(0, count($colors)-1)];
-        }
+        return $this->getRandColor();
     }
 }
