9934a6da293be4bb47ae311634e4c60fcefd314f
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2016-05-18 08:27:15 +0200

    [TASK] ADDED the basic graph class

---
+++ src/Sseidelmann/Typo3DependencyVisualization/Graph.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class Graph
+ * @package Sseidelmann\Typo3DependencyVisualization
+ * @author Sebastian Seidelmann <sebastian.seidelmann@googlemail.com>
+ */
+
+namespace Sseidelmann\Typo3DependencyVisualization;
+
+/**
+ * Class Graph
+ * @package Sseidelmann\Typo3DependencyVisualization
+ * @author Sebastian Seidelmann <sebastian.seidelmann@googlemail.com>
+ */
+class Graph
+{
+
+}
